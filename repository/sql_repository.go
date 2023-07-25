54ee6fd95f06b6f4e5c42e2e7f5beced3bbf37d1
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-07-25 23:27:25 +0200

    feat(sqlRepositoy): implement SaveTags

--- repository/sql_repository.go
+++ repository/sql_repository.go
@@ -51,7 +51,40 @@ func (r sqlRepository) SaveMusicRecord(post types.MusicPost) (int64, error) {
 	return recordId, nil
 }
 
-func SaveTags(int, []string) error {
+func (r sqlRepository) SaveTags(musicRecordId int64, tags []string) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(
+		`
+			insert into playbot_tags (
+				id,
+				tag
+			)
+			values (?, ?)
+			on duplicate key update id=id
+		`,
+	)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, tag := range tags {
+		_, err := stmt.Exec(musicRecordId, tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+
 	return nil
 }
 
