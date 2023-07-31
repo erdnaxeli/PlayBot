e1ffd8c9e8f4dcbcc3895736d33aaab9eeb6ca25
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-07-31 20:59:53 +0200

    feat(cli): better errors messages

--- playbot/save_music_record.go
+++ playbot/save_music_record.go
@@ -1,6 +1,10 @@
 package playbot
 
-import "github.com/erdnaxeli/PlayBot/types"
+import (
+	"fmt"
+
+	"github.com/erdnaxeli/PlayBot/types"
+)
 
 // Save a music record pointed by the URL in the given message.
 //
@@ -13,18 +17,23 @@ func (p *Playbot) SaveMusicRecord(
 		return 0, err
 	}
 
-	recordId, err := p.repository.SaveMusicRecord(types.MusicPost{
+	recordId, err := p.repository.SaveMusicPost(types.MusicPost{
 		MusicRecord: musicRecord,
 		Person:      person,
 		Channel:     channel,
 	})
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("error while saving music record: %w", err)
 	}
 
 	return recordId, nil
 }
 
 func (p *Playbot) SaveTags(musicRecordId int64, tags []string) error {
-	return p.repository.SaveTags(musicRecordId, tags)
+	err := p.repository.SaveTags(musicRecordId, tags)
+	if err != nil {
+		return fmt.Errorf("error while saving tags: %w", err)
+	}
+
+	return nil
 }
