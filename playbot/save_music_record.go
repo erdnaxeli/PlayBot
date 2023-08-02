82bf3b83cf653e5c6ae9a9c016e18dbf5dbe9343
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-08-02 19:11:20 +0200

    feat: return a "+" sign in front of the id if the record is new

--- playbot/save_music_record.go
+++ playbot/save_music_record.go
@@ -6,25 +6,27 @@ import (
 	"github.com/erdnaxeli/PlayBot/types"
 )
 
-// Save a music record pointed by the URL in the given message.
+// Save a music record pointed by the URL in the given message, and a post of this
+// record by the given user in the given channel.
 //
-// Return the matched URL (if any), and an error.
+// Return the id, the music record, an bool indicating if the record is a new one,
+// and an error.
 func (p *Playbot) SaveMusicRecord(
 	msg string, person types.Person, channel types.Channel,
-) (int64, types.MusicRecord, error) {
+) (int64, types.MusicRecord, bool, error) {
 	_, musicRecord, err := p.extractor.Extract(msg)
 	if err != nil {
-		return 0, types.MusicRecord{}, err
+		return 0, musicRecord, false, err
 	}
 
-	recordId, err := p.repository.SaveMusicPost(types.MusicPost{
+	recordId, isNew, err := p.repository.SaveMusicPost(types.MusicPost{
 		MusicRecord: musicRecord,
 		Person:      person,
 		Channel:     channel,
 	})
 	if err != nil {
-		return 0, types.MusicRecord{}, fmt.Errorf("error while saving music record: %w", err)
+		return recordId, types.MusicRecord{}, isNew, fmt.Errorf("error while saving music record: %w", err)
 	}
 
-	return recordId, musicRecord, nil
+	return recordId, musicRecord, isNew, nil
 }
