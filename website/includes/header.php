e0a814e9c01b4d5ef00ef975e8f2d196f876d46c
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-09-04 18:34:36 +0200

    fix

--- website/includes/header.php
+++ website/includes/header.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="utf-8">
 		<title>PlayBot</title>
-		<link rel="stylesheet" type="text/css" href="/links/common.css">
-		<script type="text/javascript" src="/links/js/jquery-1.8.2.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="/common.css">
+		<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
 		<script src="http://www.youtube.com/player_api"></script>
-		<script type="text/javascript" src="/links/js/main.js"></script>
+		<script type="text/javascript" src="/js/main.js"></script>
 	</head>
 	<body>
