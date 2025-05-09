f31896fe7f26cf742f82ad8816720bc401a21891
Alexandre Morignot <erdnaxeli@cervoi.se>
2025-05-09 19:29:42 +0200

    chore: commit the generated code

---
+++ irc/commands_test.go
@@ -0,0 +1,32 @@
+package irc_test
+
+import (
+	"fmt"
+	"testing"
+	"testing/synctest"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPrivsg(t *testing.T) {
+	synctest.Run(func() {
+		conn, _, server := connect(t)
+
+		to := gofakeit.FirstName()
+		msg := gofakeit.Sentence(5)
+		go func() {
+			err := conn.Privmsg(to, msg)
+			require.Nil(t, err)
+		}()
+
+		expected := fmt.Sprintf("PRIVMSG %s :%s\r\n", to, msg)
+		buffer := make([]byte, len(expected))
+		n, err := server.Read(buffer)
+		require.Equal(t, len(buffer), n)
+		require.Nil(t, err)
+
+		assert.Equal(t, expected, string(buffer))
+	})
+}
